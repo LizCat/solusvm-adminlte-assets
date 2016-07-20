b076acb0592325983556a634534a6dbaf7b2cc42
Balthild Ires <ibalthild@gmail.com>
2016-07-20 06:39:58 -0400

    Logged in as Administrator notice

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ jQuery(document).ready(function($) {
 		$('#actionLink').attr('href', modals[action].url);
 		$('#actionModal').modal('show');
 	};
+
+	if ($('#floatdiv').length) {
+		var title = $('#floatdiv span strong').text();
+		var link = $('#floatdiv a').attr('href');
+		var linktext = $('#floatdiv a').text();
+		$('#loginAsAdmin h4').text(title);
+		$('#loginAsAdmin a').attr('href', link).text(linktext);
+		$('#loginAsAdmin').show();
+		$('#floatdiv').hide().remove();
+	}
 });
